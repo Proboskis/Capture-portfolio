bdda16f085fd1fadf9b6a014a25d6f605ed9d86e
Proboskis <nikolasurla22@gmail.com>
2022-09-23 14:44:05 +0200

    1. First commit.

---
+++ src/components/AboutSection.js
@@ -0,0 +1,35 @@
+import home1 from "../img/home1.png";
+import { About, Description, Image, Hide } from "../styles";
+
+const AboutSection = () => {
+  return (
+    <About>
+      <div />
+      <Description>
+        <div className="title">
+          <Hide>
+            <h2>We work to make</h2>
+          </Hide>
+          <Hide>
+            <h2>
+              your <span>dreams</span> come
+            </h2>
+          </Hide>
+          <Hide>
+            <h2>true.</h2>
+          </Hide>
+        </div>
+        <p>
+          Contact us for any photography or videography ides that you have. We
+          have professionals with amazing skills.
+        </p>
+        <button>Contact us</button>
+      </Description>
+      <Image>
+        <img src={home1} alt="guy with a camera" />
+      </Image>
+    </About>
+  );
+};
+
+export default AboutSection;
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,16 @@
+// Page Component
+import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
+
+const AboutUs = () => {
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+      <FaqSection />
+    </>
+  );
+};
+
+export default AboutUs;
