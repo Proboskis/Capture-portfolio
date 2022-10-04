55c344f6e8d1e66db56a3d619ec8d09b1b7fa75e
Proboskis <nikolasurla22@gmail.com>
2022-10-04 12:20:42 +0200

    8. About animation && Wave animation.

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,46 +2,50 @@ import home1 from "../img/home1.png";
 import { About, Description, Image, Hide } from "../styles";
 // Framer Motion
 import {motion} from "framer-motion";
+import {titleAnim, fade, photoAnimation} from "../Animation";
+import Wave from "./Wave";
 
 const AboutSection = () => {
 
-  const titleAnimation = {
+  /*const titleAnimation = {
     hidden: {opacity: 0},
     show: {opacity: 1, transition: {duration: 2} },
-  }
-  const container = {
+  }*/
+
+  /*const container = {
     hidden: {x: 100},
     show: {x: 0, transition: {duration: 0.75, ease: "easeOut", staggerChildren: 1, when: "afterChildren"}}
-  }
+  }*/
 
   return (
     <About>
       <div />
       <Description>
-        <motion.div variants={container} initial="hidden" animate="show" className="title">
+        <motion.div /*variants={container} initial="hidden" animate="show" className="title"*/>
           <Hide>
-            <motion.h2 variants={titleAnimation}>
+            <motion.h2 variants={titleAnim}>
               We work to make
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2 variants={titleAnimation}>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2 variants={titleAnimation}>true.</motion.h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ides that you have. We
           have professionals with amazing skills.
-        </p>
-        <button>Contact us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="guy with a camera" />
+        <motion.img variants={photoAnimation} src={home1} alt="guy with a camera" />
       </Image>
+      <Wave />
     </About>
   );
 };
