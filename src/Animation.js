ea6cb0ce377c1dab7f6deb1c55fa6169577ce63b
Proboskis <nikolasurla22@gmail.com>
2022-10-04 13:19:20 +0200

    9. Our Work Animation and some minor fixes.

--- src/Animation.js
+++ src/Animation.js
@@ -7,15 +7,16 @@ export const pageAnimation = {
         opacity: 1,
         y: 0,
         transition: {
-            duration: 1,
-            when: "beforeChildren"
+            duration: 0.5,
+            when: "beforeChildren",
+            staggerChildren: 0.25
         }
     },
     exit: {
         opacity: 1,
-        y: 300,
+        // y: 300,
         transition: {
-            duration: 1,
+            duration: 0.25,
             staggerChildren: 0.5
         }
     }
@@ -45,6 +46,21 @@ export const photoAnimation = {
     show: {
         scale: 1,
         opacity: 1,
-        transition: { ease: "easeOut", duration: 1 }
+        transition: { ease: "easeOut", duration: 0.75 }
     }
+}
+
+export const lineAnimation = {
+    hidden: {width: "0%"},
+    show: {width: "100%", transition: {duration: 0.75}}
+}
+
+export const slider = {
+    hidden: {x: "-130%", skew: "45deg"},
+    show: {x: "100%", skew: "0deg", transition: {ease: "easeOut", duration: 1}}
+}
+
+export const sliderContainer = {
+    hidden: {opacity: 1},
+    show: {opacity: 1, transition: {staggerChildren: 0.15, ease: "easeOut"}}
 }
